61dae6826ebf551c0d02815cf53ae77542aa66d1
Ryan Welton <Ryan.G.Welton@gmail.com>
2013-07-06 18:23:35 -0400

    Fix build

--- project/build.scala
+++ project/build.scala
@@ -1,11 +1,24 @@
 import sbt._
 import Keys._
+import sbtandroid.AndroidPlugin
+import sbtandroid.AndroidPlugin._
 
 object AndroidBuild extends Build {
   lazy val main = Project(id = "main", base = file("."), settings = mainSettings)
+  
   lazy val tests = Project(id = "tests", base = file("tests"))
                    .dependsOn(main % "provided")
-  lazy val mainSettings = Defaults.defaultSettings ++ Seq(
-  	  libraryDependencies ++= Seq("org.scaloid" %% "scaloid" % "2.1-8")  
-  	)
+
+  lazy val mainSettings = Defaults.defaultSettings ++ AndroidPlugin.androidDefaults ++ Seq(
+  	  name := "CardLibrary",
+      version := "0.1",
+      versionCode := 0,
+      scalaVersion := "2.10.2",
+      platformName := "android-16",
+  	  libraryDependencies ++= Seq("org.scaloid" %% "scaloid" % "2.1-8",
+  	  							  "org.scalatest" %% "scalatest" % "1.9.1" % "test"),
+	  javacOptions ++= Seq("-encoding", "UTF-8", "-source", "1.6", "-target", "1.6"),
+      keyalias := "change-me"
+    )
+
 }
